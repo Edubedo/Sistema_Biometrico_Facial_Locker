8bba327b9d4f4f70aa4a88a1e2a0d4dd52ad2230
Edubedo <ezescobedo27@gmail.com>
2026-02-19 17:58:10 -0600

    cambios

--- listaPermitidos.py
+++ listaPermitidos.py
@@ -1,16 +1,11 @@
 class flabianos:
-    """ Lista de invitados a la cena del señor en el laboratorio """
+    """ Lista de admin """
 
     def __init__(self):
-        self.Invitados=['Luis Sustaita','Ricardo Mirón Torres',
-               'Aldo Fernando Olmeda','Paco López Ortiz','Oliver A. López',
-               'MD Diaz','Cristofer Nava','Miguel Aguirre','Polo A. Ruiz','Rodolfo Ferro',
-               'Juan Manuel Rocha','Ernesto Ramírez de Sancristobal',
-               'Adolfo Ramírez de Sancristobal','Omar Jair Purata Funes',
-               'Anthony Stark','Daniel PV','Jonathan Ivan', 'eduardo']
+        self.Administradores=['eduardo']
 
     def TuSiTuNo(self,EllosSi):        
-        if EllosSi in self.Invitados:
+        if EllosSi in self.Adnubust:
             print('Bienvenido {}'.format(EllosSi))
         else:
-            print('Lo siento {}, aun no trais el omnitrix'.format(EllosSi))
+            print('Lo siento {}, no estás en la lista de Adnubust.'.format(EllosSi))
