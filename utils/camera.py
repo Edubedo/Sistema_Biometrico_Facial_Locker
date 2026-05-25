76648124e4718785439161505692a4aca1612a6c
= <ezescobedo27@gmail.com>
2026-05-24 23:17:41 -0600

    wduwd

--- utils/camera.py
+++ utils/camera.py
@@ -336,10 +336,10 @@ class CamThread(QThread):
         # estricto con impostores. 2 frames de confirmación = fluido y seguro.
         n = len(self.labels)
         if n <= 1:
-            self._recog_threshold  = 80   # 1 usuario: puede tolerar algo más de variación
+            self._recog_threshold  = 77   # 1 usuario: puede tolerar algo más de variación
             self._recog_min_frames = 2
         elif n <= 3:
-            self._recog_threshold  = 80   # más usuarios = más riesgo de confusión
+            self._recog_threshold  = 77   # más usuarios = más riesgo de confusión
             self._recog_min_frames = 2
         elif n <= 6:
             self._recog_threshold  = 74
